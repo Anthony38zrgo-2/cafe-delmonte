74248ffa8935a79f528393d40b746f39aedaf4c5
Anthony38zrgo-2 <zrgo38@hotmail.com>
2026-07-17 16:34:18 -0500

    fix: cambio en el vite config a cafe-delmonte

--- vite.config.js
+++ vite.config.js
@@ -5,7 +5,7 @@ import { fileURLToPath, URL } from 'node:url'
 
 export default defineConfig(({ command }) => ({
   base: command === 'build'
-    ? '/cafe-del-monte/'
+    ? '/cafe-delmonte/'
     : '/',
 
   plugins: [
